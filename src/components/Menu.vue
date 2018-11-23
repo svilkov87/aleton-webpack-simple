882583fd4c407a66e655294a2ab61665934a4505
Сергей Вилков <vilkov@reg.ru>
2018-11-23 20:53:14 +0300

    Store and vuex

---
+++ src/components/Menu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="b-container">
+    <ul class="b-nav-list">
+        <li class="b-nav-list__item"
+            v-for="data in menuItems"
+            :key="data.name"
+            v-bind:class="{
+                'b-nav-list__item-active':data.active === true
+                }"
+            >
+            {{ data.name }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+    data() {
+        return {
+            menuItems : [
+                {
+                    name: 'Item_1',
+                    active : true
+                },
+                {
+                    name: 'Item_2',
+                    active : false
+                },
+                {
+                    name: 'Item_3',
+                    active : false
+                },
+                {
+                    name: 'Item_4',
+                    active : false
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+    .b-nav-list {
+        display: flex;
+
+        &__item-active {
+            color: red;
+        }
+    }
+
+</style>
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import Vue from 'vue';
-import App from './App.vue'
+import App from './App.vue';
+import Menu from './components/Menu.vue';
 
 // $(function () {
 
@@ -11,6 +12,11 @@ import App from './App.vue'
 new Vue({
     el: '#app',
     render: h => h(App)
+})
+
+new Vue({
+    el: '.b-nav',
+    render: h => h(Menu)
 })
 
 import './scss/main.scss';
